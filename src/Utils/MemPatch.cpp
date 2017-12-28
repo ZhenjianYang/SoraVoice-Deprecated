2f25c0893e98157347aba209f6bd36fbd252e894
Zhenjian Yang <ZJ.Yang@qq.com>
2017-12-29 00:04:09 +0900

    Update MemPatch

--- src/Utils/MemPatch.cpp
+++ src/Utils/MemPatch.cpp
@@ -4,32 +4,50 @@
 
 void MemPatch::SetOld(const void * data, unsigned len)
 {
+	if (!data) len = 0;
+
 	if (len > this->len_old) {
 		delete[] data_old;
 		this->data_old = new char[len];
 	}
 
-	std::memcpy(this->data_old, data, len);
+	if (len == 0) {
+		delete[] data_old;
+		data_old = nullptr;
+	}
+	else {
+		std::memcpy(this->data_old, data, len);
+	}
 	this->len_old = len;
 }
 void MemPatch::SetOld(const char * data)
 {
-	SetOld(data, strlen(data) + 1);
+	unsigned len = data ? strlen(data) + 1 : 0;
+	SetOld(data, len);
 }
 
 void MemPatch::SetNew(const void * data, unsigned len)
 {
+	if (!data) len = 0;
+
 	if (len > this->len_new) {
 		delete[] data_new;
 		this->data_new = new char[len];
 	}
 
-	std::memcpy(this->data_new, data, len);
+	if (len == 0) {
+		delete[] data_new;
+		data_new = nullptr;
+	}
+	else {
+		std::memcpy(this->data_new, data, len);
+	}
 	this->len_new = len;
 }
 void MemPatch::SetNew(const char * data)
 {
-	SetNew(data, strlen(data) + 1);
+	unsigned len = data ? strlen(data) + 1 : 0;
+	SetNew(data, len);
 }
 
 MemPatch::MemPatch(const MemPatch& _Other) {
